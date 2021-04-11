ab2a37e6524f0af9a7b87e981b1ca1ef7ae28996
Blankeos <carloantonioct@gmail.com>
2021-04-11 16:36:11 +0800

    md5 hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -36,11 +36,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET_KEY,
-  encryptedFields: ["password"],
-});
-
 // Creating a model (collection) from the schema
 const User = new mongoose.model("User", userSchema);
 
@@ -60,7 +55,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
@@ -74,7 +69,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const email = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: email }, function (err, foundUser) {
     if (err) {
