2b6b9bd6fc224f024c44eb32be549354983fb07f
Blankeos <carloantonioct@gmail.com>
2021-04-11 13:31:20 +0800

    Some dependecies and wrote connection for mongoose

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
+// Express
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(
@@ -11,3 +14,33 @@ app.use(
     extended: true,
   })
 );
+
+// Mongoose
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// Routes
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.listen(3000, function () {
+  console.log("App started on port 3000");
+});
